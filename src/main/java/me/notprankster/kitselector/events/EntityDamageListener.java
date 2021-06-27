1c3cde2c9e8ef41c9701c7691df498ee80044ac6
prankster0105 <whattheheckme122@gmail.com>
2021-06-27 09:55:34 +0800

    Create Axeman kit

--- src/main/java/me/notprankster/kitselector/events/EntityDamageListener.java
+++ src/main/java/me/notprankster/kitselector/events/EntityDamageListener.java
@@ -48,6 +48,7 @@ public class EntityDamageListener implements Listener {
         }
 
         combatTagList = tempCombatList;
+        plugin.combatList = combatTagList;
     }
 
     @EventHandler
